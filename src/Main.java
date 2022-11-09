f1b0b54e5c8e405e2df991f4f15d188c4bf8f019
makeeva_m <svobinteva@yandex.ru>
2022-11-09 19:05:39 +0300

    Циклы, ДЗ1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//  Циклы ДЗ1
+        System.out.println("Зaдaние 1");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+        System.out.println("Зaдaние 2");
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+        System.out.println("Зaдaние 3");
+        for (int i = 0; i <= 17; i = i + 2) {
+            System.out.println(i);
+        }
+        System.out.println("Зaдaние 4");
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
+//  Циклы ДЗ2
+        System.out.println("Задание 1");
+        for (int year = 1904; year <= 2096; year = year + 4) {
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " год является високосным");}
+        }
+        System.out.println("Задание 2");
+        for (int i = 7; i <= 98; i = i + 7) {
+            System.out.println(i);
+        }
+        System.out.println("Задание 3");
+        for (int i = 1; i <= 512; i = i * 2) {
+            System.out.println(i);
+        }
+//  Циклы ДЗ3
+        System.out.println("Задание 1");
+        int saved = 0;
+        int savedMonth = 29000;
+        for (int i = 1; i <= 12; i++) {
+            saved = saved + saved / 12;
+            saved = saved + savedMonth;
+            System.out.println("Месяц " + i + " сумма накоплений равна " + saved + " рублей");
+        }
     }
 }
